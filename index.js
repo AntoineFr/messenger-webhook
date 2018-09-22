6ed50593751406dc8e06fe38e48e1b32691cf36e
Antoine <antoine.boursin@gmail.com>
2018-09-22 12:46:39 +0200

    Get PSID in webhook

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -10,26 +12,33 @@ const
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook
-app.post('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {  
 
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
@@ -39,7 +48,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "zeA5gWnAPKzey97X7n4L4uw82NU6U65m"
+  const VERIFY_TOKEN = "zeA5gWnAPKzey97X7n4L4uw82NU6U65m"
 
   // Parse the query params
   let mode = req.query['hub.mode'];
@@ -62,3 +71,18 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+  
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
